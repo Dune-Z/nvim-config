378b8a1262f0cba6f92a3645ef225424968c9c19
Yifei Zuo <985774836@qq.com>
2024-08-06 01:24:21 +0800

    Create init.lua

---
+++ init.lua
@@ -0,0 +1,244 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.g.have_nerd_font = false
+vim.opt.showmode = false
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.schedule(function()
+    vim.opt.clipboard = 'unnamedplus'
+end) -- reschedule to reduce startup-time
+vim.opt.breakindent = true
+vim.opt.ignorecase = true -- for search
+vim.opt.smartcase = true -- for search
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10 -- minimal number of lines below the cursor
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+vim.opt.background = 'dark'
+
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
+-- Visual mode mappings for continuous indentation
+vim.api.nvim_set_keymap('v', '>', '>gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<', '<gv', { noremap = true, silent = true })
+-- Return to the last edit position when reopening a file
+vim.api.nvim_create_autocmd('BufReadPost', {
+    pattern = '*',
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        local line_count = vim.api.nvim_buf_line_count(0)
+        if mark[1] > 0 and mark[1] <= line_count then
+            vim.api.nvim_win_set_cursor(0, mark)
+        end
+    end,
+})
+
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.uv.fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then
+    error('Error cloning lazy.nvim:\n' .. out)
+  end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+    -- colorsheme that will be used when installing plugins
+    spec = {
+        {
+            'Mofiqul/vscode.nvim',
+            priority = 1000,
+            transparent = true,
+            italic_comments = false,
+            underline_links = true,
+            disable_nvimtree_bg = true,
+            color_overrides = { vscLineNumber = '#FFFFFF' },
+            init = function()
+                -- vim.cmd.colorscheme 'vscode'
+            end,
+        },
+        {
+            "neanias/everforest-nvim",
+            version = false,
+            lazy = false,
+            priority = 1000,
+            config = function()
+                require("everforest").setup({
+                    background = "hard",
+                    transparent_background_level = 0,
+                    italics = false,
+                    disable_italic_comments = true,
+                    sign_column_background = "none",
+                    ui_contrast = "low",
+                    dim_inactive_windows = false,
+                    diagnostic_text_highlight = false,
+                    diagnostic_virtual_text = "coloured",
+                    diagnostic_line_highlight = false,
+                    spell_foreground = false,
+                    show_eob = true,
+                    float_style = "bright",
+                    inlay_hints_background = "none",
+                    on_highlights = function(highlight_groups, palette) end,
+                    colours_override = function(palette) end,
+                })
+            end,
+            init = function()
+                vim.cmd.colorscheme 'everforest'
+            end,
+        },
+        {
+            'folke/todo-comments.nvim',
+            event = 'VimEnter',
+            dependencies = { 'nvim-lua/plenary.nvim' },
+            opts = { signs = false }
+        },
+        {
+            'nvim-lualine/lualine.nvim',
+            dependencies = { 'nvim-tree/nvim-web-devicons' }
+        },
+        {
+            'nvim-treesitter/nvim-treesitter',
+            build = ':TSUpdate',
+            opts = {
+                ensure_installed = { 'cpp', 'c', 'python', 'rust', 'lua', 'bash' },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = { 'ruby' },
+                },
+                indent = { enable = true, disable = { 'ruby' } },
+            },
+            config = function(_, opts)
+                ---@diagnostic disable-next-line: missing-fields
+                require('nvim-treesitter.configs').setup(opts)
+            end,
+        },
+        { -- Fuzzy Finder (files, lsp, etc)
+            'nvim-telescope/telescope.nvim',
+            event = 'VimEnter',
+            branch = '0.1.x',
+            dependencies = {
+                'nvim-lua/plenary.nvim',
+                {
+                    'nvim-telescope/telescope-fzf-native.nvim',
+                    build = 'make',
+                    cond = function()
+                        return vim.fn.executable 'make' == 1
+                    end,
+                },
+                { 'nvim-telescope/telescope-ui-select.nvim' },
+                { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+            },
+            config = function()
+                require('telescope').setup {
+                extensions = {
+                    ['ui-select'] = {
+                        require('telescope.themes').get_dropdown(),
+                    },
+                },
+            }
+            -- Enable Telescope extensions if they are installed
+            pcall(require('telescope').load_extension, 'fzf')
+            pcall(require('telescope').load_extension, 'ui-select')
+
+            -- See `:help telescope.builtin`
+            local builtin = require 'telescope.builtin'
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+            vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+            vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+            vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+            vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+            vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+            vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+            -- Slightly advanced example of overriding default behavior and theme
+            vim.keymap.set('n', '<leader>/', function()
+                -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+                builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                    winblend = 10,
+                    previewer = false,
+                })
+            end, { desc = '[/] Fuzzily search in current buffer' })
+
+            -- It's also possible to pass additional configuration options.
+            --  See `:help telescope.builtin.live_grep()` for information about particular keys
+            vim.keymap.set('n', '<leader>s/', function()
+            builtin.live_grep {
+                grep_open_files = true,
+                prompt_title = 'Live Grep in Open Files',
+            }
+            end, { desc = '[S]earch [/] in Open Files' })
+            -- Shortcut for searching your Neovim configuration files
+            vim.keymap.set('n', '<leader>sn', function()
+            builtin.find_files { cwd = vim.fn.stdpath 'config' }
+            end, { desc = '[S]earch [N]eovim files' })
+        end,
+        },
+        {
+            'neovim/nvim-lspconfig',
+            dependences = {
+                { 'williamboman/mason.nvim', config = true },
+                'williamboman/mason-lspconfig.nvim',
+                'WhoIsSethDaniel/mason-tool-installer.nvim',
+                { 'j-hui/fidget.nvim', opts = {} },
+                'hrsh7th/cmp-nvim-lsp',
+            },
+            config = function()
+                require'lspconfig'.pyright.setup{}
+                require'lspconfig'.lua_ls.setup{}
+                require'lspconfig'.rust_analyzer.setup{}
+                require'lspconfig'.clangd.setup{}
+            end,
+        }
+    },
+    -- update checker
+    checker = { enabled = true },
+})
+
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
